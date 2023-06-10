cf43e00be16edf52a40b3ffb42d1751d88054bbc
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 09:37:54 +0800

    Create build.sh

---
+++ src/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# dynr master
+
+git clone https://github.com/mhunter1/dynr.git
+cd dynr
+./configure
+make clean install
+cd ..
+rm -rf dynr
+Rscript -e "demo('LinearSDE', package = 'dynr')"
+
+# dynr arma
+
+git clone -b arma https://github.com/mhunter1/dynr.git
+cd dynr
+./configure
+make clean install
+cd ..
+rm -rf dynr
+Rscript -e "demo('LinearSDE', package = 'dynr')"
