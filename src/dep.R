51c0597b28b26536fe37cb2027b182b3a12a1c65
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 10:54:59 +0800

    Update dep.R

--- src/dep.R
+++ src/dep.R
@@ -9,7 +9,6 @@ dep <- read.csv(
 for (i in seq_along(dep)) {
   install.packages(
     dep[i],
-    repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/jammy/latest"),
-    lib = '/usr/local/lib/R/library'
+    repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/jammy/latest")
   )
 }
