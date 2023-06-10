b29c1e7fd109afd020a6e51178174b1d084c6b73
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 21:08:30 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -2,11 +2,28 @@
 
 set -e
 
-mkdir dynr
+# roxygen2
+
+Rscript -e "                                              \
+    remotes::install_version(                             \
+        package = 'roxygen2',                             \
+        version = '5.0.1',                                \
+        repos = c(CRAN = 'https://cran.rstudio.com')      \
+    )                                                     \
+"
+
+# tinytex
+
+Rscript -e "                                              \
+    try(tinytex::install_tinytex())                       \
+"
+
+git clone https://github.com/mhunter1/dynr.git
 cd dynr
-mkdir src
-cd src
-touch Makevars.in
-wget -q https://raw.githubusercontent.com/sigmaresearch100/r-ubuntu-22.04-dynr/main/src/Makevars.in -O Makevars.in
-ls
-cat Makevars.in
+./configure
+make clean install
+cd ..
+rm -rf dynr
+Rscript -e "demo('LinearSDE', package = 'dynr')"
+
+echo -e "\nInstall dynr, done!"
