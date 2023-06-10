c3501e4cbedfe7e7c3b8d40b00ce4a673d5a6465
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 20:58:20 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -7,6 +7,6 @@ cd dynr
 mkdir src
 cd src
 touch Makevars.in
-wget -q https://raw.githubusercontent.com/sigmaresearch100/r-ubuntu-22.04-dynr/main/src/Makevars.in
+wget -q https://raw.githubusercontent.com/sigmaresearch100/r-ubuntu-22.04-dynr/main/src/Makevars.in -O Makevars.in
 ls
 cat Makevars.in
