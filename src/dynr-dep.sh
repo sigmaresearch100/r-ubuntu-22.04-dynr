d383d4f5d694ba24297038ae39a71e38efe40bff
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 08:52:19 +0800

    Update dynr-dep.sh

--- src/dynr-dep.sh
+++ src/dynr-dep.sh
@@ -3,7 +3,17 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y            \
-    libcurl4-openssl-dev           \
-    libharfbuzz-dev libfribidi-dev \
-    libfontconfig1-dev
+sudo apt-get install -y    \
+    libcurl4-openssl-dev   \
+    libharfbuzz-dev        \
+    libfribidi-dev         \
+    libfontconfig1-dev     \
+    build-essential        \
+    cmake                  \
+    libopenblas-dev        \
+    liblapack-dev          \
+    libarpack2-dev         \
+    libsuperlu-dev         \
+    libgsl-dev             \
+    libarmadillo-dev
+ 
