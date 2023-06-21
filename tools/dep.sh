f8be1e7f03e60a867ae9aa4da1c0bbc264c9147a
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-22 01:03:46 +0800

    freetype2

--- tools/dep.sh
+++ tools/dep.sh
@@ -3,20 +3,20 @@
 set -e
 
 apt-get update
-apt-get install -y       \
-    libcurl4-openssl-dev \
-    libharfbuzz-dev      \
-    libfribidi-dev       \
-    libfontconfig1-dev   \
-    build-essential      \
-    cmake                \
-    libopenblas-dev      \
-    liblapack-dev        \
-    libarpack2-dev       \
-    libsuperlu-dev       \
-    libgsl-dev           \
-    libarmadillo-dev     \
-    libeigen3-dev
+#apt-get install -y       \
+#    libcurl4-openssl-dev \
+#    libharfbuzz-dev      \
+#    libfribidi-dev       \
+#    libfontconfig1-dev   \
+#    build-essential      \
+#    cmake                \
+#    libopenblas-dev      \
+#    liblapack-dev        \
+#    libarpack2-dev       \
+#    libsuperlu-dev       \
+#    libgsl-dev           \
+#    libarmadillo-dev     \
+#    libeigen3-dev
 
 # freetype2
 apt-get install -y   \
